cdcad0c90ae2d514fe322ea279f05052a8c9f020
Terence147 <terencetan147@gmail.com>
2025-04-17 00:18:25 +0800

    ApplicationController

---
+++ src/group4_sc2002_project/ApplicationController.java
@@ -0,0 +1,51 @@
+import java.time.LocalDate;
+
+public class ApplicationController {
+
+    public boolean applyForProject(Applicant applicant, Project project, String flatType) 
+    {
+        if (!canApplyFor(applicant, project)) {
+            return false;
+        }
+
+        int generatedId = 0; //you can cook the hash thingy u wanted
+        Application application = new Application(generatedId, applicant, project, flatType, ApplicationStatus.Pending);
+
+        applicant.setApplication(application);
+        return true;
+    }
+
+  
+    public boolean canApplyFor(Applicant applicant, Project project) 
+    {
+        if (applicant.viewApplication() != null) 
+        {
+            return false;
+        }
+
+        LocalDate today = LocalDate.now();
+        return project.isWithinApplicationPeriod(today);
+    }
+
+    public void withdrawApplication(Applicant applicant) 
+    {
+        Application applic = applicant.viewApplication();
+        if (applic != null && !applic.isWithdrawalRequested()) 
+        {
+            applic.setPendingWithdrawal(true);
+            applic.setStatus(ApplicationStatus.Withdrawal_Requested);
+        }
+    }
+
+    public ApplicationStatus getApplicationStatus(Applicant applicant) 
+    {
+        Application applic = applicant.viewApplication();
+        if (applic != null) 
+        {
+            return applic.getStatus();
+        }
+
+        return null;
+    }
+
+}
