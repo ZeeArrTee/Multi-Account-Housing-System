ffe9a1915bec13cab3821d1b50c77e34296c48f6
Ansel-Kee <a1.kee.jx@gmail.com>
2025-04-18 17:37:59 +0800

    Fixed ApplicationController

--- src/group4_sc2002_project/ApplicationController.java
+++ src/group4_sc2002_project/ApplicationController.java
@@ -5,12 +5,41 @@ import java.time.LocalDate;
 public class ApplicationController {
 	private int appCount = 0;
 
+	public boolean applyForProject(Applicant applicant, Project project, String flatType) {
+		if (!canApplyFor(applicant, project)) {
+			return false;
+		}
+// you can cook the hash thingy u wanted
 
-<<<<<<< Updated upstream
-        int generatedId = 0; //you can cook the hash thingy u wanted
+		applicant.applyForProject(project, flatType, appCount);
+		appCount++;
+		return true;
+	}
 
-        applicant.applyForProject(project, flatType, nextApplicationId++);
-        return true;
-    }
+	public boolean canApplyFor(Applicant applicant, Project project) {
+		if (applicant.viewApplication() != null) {
+			return false;
+		}
+
+		LocalDate today = LocalDate.now();
+		return project.isWithinApplicationPeriod(today);
+	}
+
+	public void withdrawApplication(Applicant applicant) {
+		Application applic = applicant.viewApplication();
+		if (applic != null && !applic.isWithdrawalRequested()) {
+			applic.setPendingWithdrawal(true);
+			applic.setStatus(ApplicationStatus.Withdrawal_Requested);
+		}
+	}
+
+	public ApplicationStatus getApplicationStatus(Applicant applicant) {
+		Application applic = applicant.viewApplication();
+		if (applic != null) {
+			return applic.getStatus();
+		}
+
+		return null;
+	}
 
 }
