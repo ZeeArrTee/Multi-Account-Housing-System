9bc14443c03133081719deb7b5e8c18a9ea43ff9
Ansel-Kee <a1.kee.jx@gmail.com>
2025-04-24 18:21:49 +0800

    added options to the menu

--- src/group4_sc2002_project/MainMenu.java
+++ src/group4_sc2002_project/MainMenu.java
@@ -13,7 +13,12 @@ public class MainMenu {
 		User check = null;
 		int choice;
 		do {
-			System.out.println("Login Page:\nEnter your choice:\n1) Login\n2) Create User\n3) Reset Password\n4) Exit");
+			System.out.println("Login Page:");
+			System.out.println("Enter your choice:");
+			System.out.println("1) Login");
+			System.out.println("2) Create User");
+			System.out.println("3) Reset Password");
+			System.out.println("4) Exit");
 			choice = s.nextInt();
 			switch (choice) {
 			case 1:
@@ -56,7 +61,7 @@ public class MainMenu {
 						System.out.println("Invalid marital status!");
 					}
 				}
-				check = service.createUser(name, userID, age, maritalStatus, password,"User");
+				check = service.createUser(name, userID, age, maritalStatus, password, "User");
 				if (check != null) {
 					return check;
 				}
@@ -78,11 +83,81 @@ public class MainMenu {
 		return check;
 	}
 
-	public static void viewProjects() {
+	public static void viewProjects(User user) {
+	}
+
+	public static void viewApplications(User user) {
 
 	}
 
-	public static void viewApplications() {
+	public static void managerDashboard(User user) {
+		String userID, password;
+		Manager manager = (Manager) user;
+		ManagerDisplay display = new ManagerDisplay(manager);
+		int choice;
+		do {
+			System.out.println("Manager Dashboard:");
+			System.out.println("Enter your choice:");
+			System.out.println("1) Project Menu");
+			System.out.println("2) Officer Registration Menu");
+			System.out.println("3) Application Menu");
+			System.out.println("4) Enquiry Menu");
+			System.out.println("5) Exit");
+			choice = s.nextInt();
+			switch (choice) {
+			case 1:
+
+				break;
+			case 2:
+				List<String> married = new ArrayList<String>(2);
+				married.add("Married");
+				married.add("Single");
+				int age = 0;
+				String name = "\0";
+				String maritalStatus = "\0";
+				System.out.println("Key in your Name: (Key in # to go back)");
+				name = s.next();
+				if (name.equals("#")) {
+					break;
+				}
+				System.out.println("Key in your userID: ");
+				userID = s.next();
+				System.out.println("Key in your password:");
+				password = s.next();
+				System.out.println("Key in your age:");
+				age = s.nextInt();
+				while (!married.contains(maritalStatus)) {
+					System.out.println("Key in your marital status(Single/Married):");
+					maritalStatus = s.next();
+					if (!married.contains(maritalStatus)) {
+						System.out.println("Invalid marital status!");
+					}
+				}
+				check = service.createUser(name, userID, age, maritalStatus, password, "User");
+				if (check != null) {
+					return check;
+				}
+			case 3:
+				System.out.println("Key in your userID: (Key in # to go back)");
+				userID = s.next();
+				if (userID.equals("#")) {
+					break;
+				}
+				System.out.println("Key in your new password:");
+				password = s.next();
+				service.changePassword(userID, password);
+				break;
+			case 4:
+				break;
+			case 5:
+				System.out.println("Returning to main menu...");
+				break;
+			}
+		} while (choice < 4);
+
+	}
+
+	public static void officerDashboard() {
 
 	}
 
@@ -91,16 +166,23 @@ public class MainMenu {
 		int choice;
 		User user = null;
 		do {
-			System.out.println("Main Page:\nEnter your choice:\n1) Login\n2) Projects\n3) Applications\n4) Exit\n");
+			System.out.println("Main Page:");
+			System.out.println("Enter your choice:");
+			System.out.println("1) Login");
+			System.out.println("2) Projects");
+			System.out.println("3) Applications");
+			System.out.println("4) Officer Dashboard");
+			System.out.println("5) Manager Dashboard");
+			System.out.println("6) Exit");
+			System.out.println();
 			if (user == null) {
 				System.out.println("Currently logged out.");
-			}
-			else {
+			} else {
 				System.out.println("Currently logged in as: " + user.getName());
-				System.out.println("Role: " + user.getRole().get(user.getRole().size()-1));
+				System.out.println("Role: " + user.getRole().get(user.getRole().size() - 1));
 			}
 			choice = s.nextInt();
-			String log; 
+			String log;
 			switch (choice) {
 			case 1:
 				if (user != null) {
@@ -119,16 +201,35 @@ public class MainMenu {
 					System.out.println("Login to access!");
 					break;
 				}
-				viewProjects();
+				viewProjects(user);
 				break;
 			case 3:
 				if (user == null) {
 					System.out.println("Login to access!");
 					break;
 				}
-				viewApplications();
+				viewApplications(user);
 				break;
 			case 4:
+				if (user == null) {
+					System.out.println("Login to access!");
+					break;
+				} else if (!user.getRole().contains("Officer")) {
+					System.out.println("Access Denied");
+					break;
+				}
+				officerDashboard();
+				break;
+			case 5:
+				if (user == null) {
+					System.out.println("Login to access!");
+					break;
+				} else if (!user.getRole().contains("Manager")) {
+					System.out.println("Access Denied");
+					break;
+				}
+				managerDashboard(user);
+			case 6:
 				System.out.println("Exiting...");
 				break;
 			}
